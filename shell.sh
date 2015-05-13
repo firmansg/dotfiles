9810d5266d4a502bea8f4a0c3fe8fcba40ac6dd8
Taylor Otwell <taylorotwell@gmail.com>
2015-05-12 23:59:52 -0500

    first

---
+++ shell.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Aliases
+
+alias gs='git status -s'
+alias code='cd ~/Documents/Code'
+alias dots='st ~/.dotfiles/shell.sh'
+alias vm='ssh vagrant@127.0.0.1 -p 2222'
+alias he='st ~/.homestead/Homestead.yaml'
+alias laravel='cd ~/Documents/Code/Laravel/laravel'
+alias framework='cd ~/Documents/Code/Laravel/framework'
+alias hp='cd ~/Documents/Code/Homestead && vagrant provision'
+
+# Copy laravel/framework to laravel/laravel vendor directory...
+
+laracopy () {
+	rm -rf /Users/Taylor/Documents/Code/Laravel/laravel/vendor/laravel/framework
+	rsync --quiet -r /Users/Taylor/Documents/Code/Laravel/framework/* /Users/Taylor/Documents/Code/Laravel/laravel/vendor/laravel/framework
+	echo "Copied!"
+}
