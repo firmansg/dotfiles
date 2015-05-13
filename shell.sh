eb3414ace057cbda766a83b818a3f9800d97b95a
Taylor Otwell <taylorotwell@gmail.com>
2015-05-13 00:08:37 -0500

    updates

--- shell.sh
+++ shell.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Path
+
+export PATH="$PATH:/Users/Taylor/.composer/vendor/bin"
+
 # Aliases
 
 alias gs='git status -s'
@@ -7,7 +11,7 @@ alias code='cd ~/Documents/Code'
 alias dots='st ~/.dotfiles/shell.sh'
 alias vm='ssh vagrant@127.0.0.1 -p 2222'
 alias he='st ~/.homestead/Homestead.yaml'
-alias laravel='cd ~/Documents/Code/Laravel/laravel'
+alias lara='cd ~/Documents/Code/Laravel/laravel'
 alias framework='cd ~/Documents/Code/Laravel/framework'
 alias hp='cd ~/Documents/Code/Homestead && vagrant provision'
 
