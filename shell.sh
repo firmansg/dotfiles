0c0c55ea870ac5ad118c1aa7142488677f0a4314
Taylor Otwell <taylorotwell@gmail.com>
2015-05-14 17:32:12 -0500

    source bashmarks

--- shell.sh
+++ shell.sh
@@ -4,16 +4,17 @@
 
 export PATH="$PATH:/Users/Taylor/.composer/vendor/bin"
 
+# Source Bashmarks
+
+source ~/.bashmarks/bin/bashmarks.sh
+
 # Aliases
 
 alias gs='git status -s'
-alias code='cd ~/Documents/Code'
 alias dots='st ~/.dotfiles/shell.sh'
 alias vm='ssh vagrant@127.0.0.1 -p 2222'
 alias he='st ~/.homestead/Homestead.yaml'
 alias nah='git reset --hard; git clean -df;'
-alias lara='cd ~/Documents/Code/Laravel/laravel'
-alias framework='cd ~/Documents/Code/Laravel/framework'
 alias hp='cd ~/Documents/Code/Homestead && vagrant provision'
 
 # Copy laravel/framework to laravel/laravel vendor directory...
